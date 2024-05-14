442795f5d37012bde6e3eb7549885cb23c329dd1
joshdellamuth <73405763+joshdellamuth@users.noreply.github.com>
2024-05-13 21:35:46 -0500

    1. Deleted Default Files 2. Finished Dark Mode and it persists acorss refresh w/ ThemeConfigurationService 3. Made the NavBar organization

---
+++ MerechatBlazorUI/MerechatBlazorUI/Services/ThemeConfigurationService.cs
@@ -0,0 +1,12 @@
+﻿namespace MeerchatBlazorUI.Data
+{
+    public class ThemeConfigurationService
+    {
+        public bool isDarkMode { get; set; } = false;
+
+        public void SwitchDarkMode()
+        {
+            isDarkMode = !isDarkMode;
+        }
+    }
+}
