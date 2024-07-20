04a7f2b27e887bc5da49fb5cbe04a6e4c9e7d888
joshdellamuth <73405763+joshdellamuth@users.noreply.github.com>
2024-07-20 12:54:16 -0500

    Adding SSIS project and initial testing APIs

--- Meerchat/Meerchat.Api.Web/Program.cs
+++ Meerchat/Meerchat.Api.Web/Program.cs
@@ -7,8 +7,6 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<Microsoft.FluentUI.AspNetCore.Components.LibraryConfiguration>();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
---
+++ Meerchat/MeerchatUI/Components/APIEntities/Weather.cs
@@ -0,0 +1,10 @@
+﻿namespace MeerchatUI.Components.APIEntities
+{
+    public class Weather
+    {
+        public int date { get; set; }
+        public int temperatureC { get; set; }
+        public int temperatureF { get; set; }
+        public string summary { get; set; }
+    }
+}
